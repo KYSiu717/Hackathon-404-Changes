8ae5d4baba062a19a0e809b87ab0ccaec8e4de52
budken <siu.afmm@gmail.com>
2018-06-28 00:08:59 -0400

    commited

--- 404-background.js
+++ 404-background.js
@@ -1,8 +1,25 @@
-// This is where the background page goes!
-// Adding a Listener to Error Occured Event
+This is where the background page goes!
+Adding a Listener to Error Occured Event
 chrome.webNavigation.onErrorOccurred.addListener(function(data) {
   // Updating the browser window with desired URL
   chrome.tabs.update(data.tabID, {
     url: chrome.extension.getURL("newPage.html")
   };
 });
+// console.log(chrome.webNavigation.onErrorOccurred)
+// var filter = {
+//   url:
+//   [
+//     {hostContains: "example.com"},
+//     {hostPrefix: "developer"}
+//   ]
+// }
+
+// function logOnErrorOccurred(details) {
+//   console.log("onErrorOccurred: " + details.url);
+//   console.log(details.error);
+// }
+
+// chrome.webNavigation.onErrorOccurred.addListener(logOnErrorOccurred, filter);
+
+
