d7cf013f63758da6626bd9418b915cca1f0e3732
budken <siu.afmm@gmail.com>
2018-06-27 22:08:24 -0400

    updated

--- 404-background.js
+++ 404-background.js
@@ -1,6 +1,7 @@
 // This is where the background page goes!
-
+// Adding a Listener to Error Occured Event
 chrome.webNavigation.onErrorOccurred.addListener(function(data) {
+  // Updating the browser window with desired URL
   chrome.tabs.update(data.tabID, {
     url: chrome.extension.getURL("newPage.html")
   };
