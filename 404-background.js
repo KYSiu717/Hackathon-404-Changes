de66e1b9e7b6067a7846a619782efcc3facf935c
budken <siu.afmm@gmail.com>
2018-06-28 19:32:25 -0400

    commited

--- 404-background.js
+++ 404-background.js
@@ -49,14 +49,16 @@
 //       alert('Success');
 //    },
 // });
-console.log("Hello")
+// console.log("Hello")
 
-var pattern=/\bBlocked/;
-var viewtext_base_url = "http://viewtext.org/article?url=";
-var newurl;
-if (pattern.test(window.document.title)) // if it matches pattern defined above
-{
-  newurl = viewtext_base_url + encodeURIComponent(window.location.href);
-  chrome.extension.sendRequest({redirect: newurl}); // send message to redirect
+// var pattern=/\bBlocked/;
+// var viewtext_base_url = "http://viewtext.org/article?url=";
+// var newurl;
+// if (pattern.test(window.document.title)) // if it matches pattern defined above
+// {
+//   newurl = viewtext_base_url + encodeURIComponent(window.location.href);
+//   chrome.extension.sendRequest({redirect: newurl}); // send message to redirect
 
-}
+// }
+
+console.log("Test to see if this is being loaded")
